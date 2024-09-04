c59739f82d51bb180810722f8553a4d95f73ac3a
João Victor Ferrareis Ribeiro <joaovito2002@gmail.com>
2024-09-04 19:26:42 -0300

    commit of dog

---
+++ dog.go
@@ -0,0 +1,7 @@
+// Package dog includes functions for human and dog year convertions
+package dog
+
+// Years  takes human years and turns them into dog years (1 human year = 7 dog years).
+func Years(age int) int {
+	return age * 7
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/JoaoFerrareis02/go-dog
+
+go 1.22.6
